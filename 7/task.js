a75efcc1aca79d41e7704a1be0cbb94dac8f2e96
AntonVoronezh <pstut@mail.ru>
2018-11-03 20:55:04 +0300

    7 #5

---
+++ 7/task.js
@@ -0,0 +1,32 @@
+function User(fullName) {
+  this.fullName = fullName;
+
+  Object.defineProperty(this, 'firstName', {
+    get() {
+      const split = this.fullName.split(' ');
+      return split[0];
+    },
+    set(arg) {
+      this.fullName = `${arg} ${this.lastName}`;
+    },
+  });
+  Object.defineProperty(this, 'lastName', {
+    get() {
+      const split = this.fullName.split(' ');
+      return split[1];
+    },
+    set(arg) {
+      this.fullName = `${this.firstName} ${arg}`;
+    },
+  });
+}
+
+const vasya = new User('Василий Попкин');
+
+// чтение firstName/lastName
+console.log(`${vasya.firstName}\n`); // Василий
+console.log(vasya.lastName); // Попкин
+
+// запись в lastName
+vasya.lastName = 'Сидоров';
+console.log(vasya.fullName); // Василий Сидоров
